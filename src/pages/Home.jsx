d5610c27a7cf16392c45ef3a979f1b7a0a9fab20
Soukaina Mouradi <soukainamouradi58@gmail.com>
2025-03-19 19:01:23 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import MovieList from "../components/MovieList";
 import Filter from "../components/Filter";
 import moviesData from "../data/movies";
-//import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
 
 const Home = () => {
   const [filter, setFilter] = useState({});
@@ -17,7 +17,22 @@ const Home = () => {
 
   return (
     <div className="p-6">
-      <h1 className="text-4xl font-bold text-center mb-6">MovieFlex</h1>
+      {/* Navbar */}
+      <nav className="bg-gray-800 text-white py-4 mb-6">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-2xl font-semibold">MovieFlex</div>
+          <ul className="flex space-x-6">
+            <li><a href="#home" className="hover:text-gray-400">Home</a></li>
+            <li><a href="#movies" className="hover:text-gray-400">Movies</a></li>
+            <li><a href="#contact" className="hover:text-gray-400">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* Hero Section */}
+      <HeroSection />
+      <br></br>
+      <h1 className="text-4xl font-bold text-center mb-6">Select your movie</h1>
       <Filter setFilter={setFilter} />
       <MovieList movies={filteredMovies} />
     </div>
